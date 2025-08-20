8e7e68d1a244843b19a3f79f090b3b34dc86617c
lelletocka <linnea.valtersson@hotmail.se>
2025-08-20 13:32:22 +0200

    add free API after testing trial and error with other API

--- mini-game/src/components/API.jsx
+++ mini-game/src/components/API.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 const API = () => {
-
-    const fetchData = async () => {
+    /* -----lesson template----- */ 
+    /*const fetchData = async () => {
         try {
             const response = await fetch(
                 ""
@@ -13,13 +13,41 @@ const API = () => {
         } catch (error) {
             console.log("Error fetching data:", error);
         }
+    };*/
+
+
+
+    /* -----test----- */
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(
+                "https://dog.ceo/api/breeds/image/random"
+            );
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.log("Error fetching data:", error);
+        }
     };
+
+
+
+
+    /* ----- */
 
     useEffect(() => {
         fetchData();        
     }, []);
 
-    return <div>API Assignment</div>;
+
+    return <div>API Assignment</div>
+
 };
+
+
+
+
+
 
 export default API;
