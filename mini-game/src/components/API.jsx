f157a0acba6e3c5ee740b140d61c6aa82ee9353c
lelletocka <linnea.valtersson@hotmail.se>
2025-08-21 08:22:13 +0200

    try different approaches for game functionality

--- mini-game/src/components/API.jsx
+++ mini-game/src/components/API.jsx
@@ -1,23 +1,15 @@
-import React from "react";
+import React, { use } from "react";
 import { useEffect, useState } from "react";
 
 const API = () => {
-    /* -----lesson template----- */ 
-    /*const fetchData = async () => {
-        try {
-            const response = await fetch(
-                ""
-            );
-            const data = await response.json();
-            console.log(data);
-        } catch (error) {
-            console.log("Error fetching data:", error);
-        }
-    };*/
 
+    const [dogImage, setDogImage] = useState({});
+    const resultText = [
+        {wrong: "You clicked the wrong button! You lost! Try again!"},
+        {right: "You clicked the right button! You won! Here is a dog image:"},
+    ];
 
-
-    /* -----test----- */
+    /*const [] = useState(false);*/
 
     const fetchData = async () => {
         try {
@@ -25,24 +17,53 @@ const API = () => {
                 "https://dog.ceo/api/breeds/image/random"
             );
             const data = await response.json();
+            setDogImage(data);            
             console.log(data);
         } catch (error) {
             console.log("Error fetching data:", error);
         }
     };
 
+    const [generateText, setGenerateText] = useState("");
+
+    /*const generateDogImage = () => {
+        fetchData();
+        
+    };*/
+
+    
+    const [youLost, setYouLost] = useState(false);
+        
+    const [youWon, setYouWon] = useState(false);
+    
+    const clickWrong = () => {
+        setYouLost(true);
+        setYouWon(false);
+        /*setGenerateText(resultText.wrong);*/
+    };
+    const clickRight = () => {
+        setYouWon(true);
+        setYouLost(false);
+        /*setGenerateText(resultText.right);*/
+        fetchData();
+    };
 
 
 
-    /* ----- */
+    return (
+        <div>
+            <h1>Dog Image Game</h1>
+            <p>Click the right button to generate random dog image:</p>
+            <button onClick={clickWrong}>Wrong button</button>
+            <button onClick={clickRight}>Right button</button>
 
-    useEffect(() => {
-        fetchData();        
-    }, []);
+            
 
-
-    return <div>API Assignment</div>
-
+            <p></p>
+            <img src={dogImage.message}/>
+            
+        </div>
+    );
 };
 
 
